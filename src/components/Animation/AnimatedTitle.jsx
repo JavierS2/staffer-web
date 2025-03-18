73447ded0e8ad5d57067cadc421fa807033ff566
JavierS <javieandres2004@gmail.com>
2025-03-18 12:08:06 -0500

    change texts and about img

--- src/components/Animation/AnimatedTitle.jsx
+++ src/components/Animation/AnimatedTitle.jsx
@@ -2,20 +2,29 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 
-export default function AnimatedTitle({ text }) {
+export default function AnimatedTitle({ text, delay = 0, typingSpeed = 25 }) {
   const [displayedText, setDisplayedText] = useState("");
   const [index, setIndex] = useState(0);
+  const [startTyping, setStartTyping] = useState(false);
   const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.5 });
 
   useEffect(() => {
-    if (inView && index < text.length) {
+    if (inView) {
+      // Espera el tiempo del delay antes de comenzar a escribir
+      const timeout = setTimeout(() => setStartTyping(true), delay);
+      return () => clearTimeout(timeout);
+    }
+  }, [inView, delay]);
+
+  useEffect(() => {
+    if (startTyping && index < text.length) {
       const timeout = setTimeout(() => {
         setDisplayedText((prev) => prev + text[index]);
         setIndex(index + 1);
-      }, 20); // Aumenta la velocidad de tipeo
+      }, typingSpeed); // Velocidad de tipeo ajustable
       return () => clearTimeout(timeout);
     }
-  }, [index, text, inView]);
+  }, [index, text, startTyping, typingSpeed]);
 
   return (
     <motion.h1
@@ -23,9 +32,10 @@ export default function AnimatedTitle({ text }) {
       className="animated-title"
       initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 10 }}
-      transition={{ duration: 0.5, ease: "easeOut" }} // Transición rápida
+      transition={{ duration: 0.5, ease: "easeOut" }}
     >
-      {displayedText} <span className="cursor">|</span>
+      {displayedText}
+      {startTyping && index < text.length && <span className="cursor">|</span>}
     </motion.h1>
   );
 }
